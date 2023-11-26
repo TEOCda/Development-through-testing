e257639ea6691195589500cf14aa9beb5b279bbb
TEOC <Foox210@yandex.ru>
2023-11-26 22:44:11 +0400

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,6 @@
 public class PhoneBook {
+
+    public static int add (String name, int number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+    @Test
+    void add() {
+        PhoneBook phoneBook = new PhoneBook();
+        Assertions.assertEquals(1,phoneBook.add("Eva", 123));
+    }
 }
